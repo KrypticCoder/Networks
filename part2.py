c3f869fdd30c761803fb4e63d48667cb6e04068b
Dimitar <dimdimvas@yahoo.com>
2016-12-01 14:40:48 -0800

    change name to simulation2.py

--- part2.py
+++ part2.py
@@ -1,3 +1,6 @@
+#Dimitar Vasilev, Matthew Quesada
+
+
 from __future__ import division
 import random
 import simpy
@@ -6,19 +9,15 @@ from matplotlib import pyplot
 
 SLOT_LENGTH = 1
 RANDOM_SEED = 29
-SIM_TIME = 500000
+SIM_TIME = 100000
 NUM_NODES = 10
 exp_throughput = []
 linear_throughput = []
 Lambda = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
 
 class node:
-    def __init__(self, env, rate, my_id):
+    def __init__(self, env, rate):
         self.env = env
-        self.success_packets = 0
-        self.flag_processing = 0
-        self.packet_number = 0
-        self.id = my_id
         self.rate = rate
         self.N = 0
         self.L = 0
@@ -28,7 +27,6 @@ class node:
         self.L -= 1
         #finish processing set attmpt to 0
         self.N = 0
-        self.success_packets += 1
         self.S += 1
   
     def packets_arrival(self, env):
@@ -36,12 +34,9 @@ class node:
         while True:
             # Infinite loop for generating packets
             yield env.timeout(random.expovariate(self.rate))
-    
             # arrival time of one packet
-            self.packet_number += 1
             self.L += 1
             
-
     def exponential_backoff(self):
         self.S = self.S + random.randint(0, 2**min(self.N, 10))
         self.N += 1
@@ -56,69 +51,73 @@ class ethernet:
         self.slot_number = 0
         self.success_slots = 0
         self.collision_slots = 0
-        self.empty_slots = 0
-        self.list_nodes = [node(env, rate, x) for x in range(NUM_NODES)]
+        self.wasted_slots = 0
+        self.list_nodes = [node(env, rate) for currNode in range(NUM_NODES)]
         self.rate = rate
         self.algorithm = algorithm 
 
     def start(self):
     #enable packet arrival at simultaneous time
-        for x in range(NUM_NODES):
-            self.env.process(self.list_nodes[x].packets_arrival(self.env))
+        for eachNode in range(NUM_NODES):
+            currentN = self.list_nodes[eachNode].packets_arrival(self.env)
+            self.env.process(currentN)
 
         while True:
+            transmissions = 0
             yield self.env.timeout(SLOT_LENGTH)
+        
+            #first index                
+            node_index = -1            
+            for current in range(NUM_NODES):
+                packets_in_queue = self.list_nodes[current].L
+                next_transmission = self.list_nodes[current].S
+                # Check if node has packets
+                # Check if behind in slots and update if so            
+                if(packets_in_queue and (next_transmission <= self.slot_number)):
+                    transmissions = transmissions + 1
+                    self.list_nodes[current].S = self.slot_number
+                    node_index = current
 
-            request = 0                 # counter used to keep track of requests at current slot
-            node_index = -1             #   index of the first request
-            for x in range(NUM_NODES):
-                if(self.list_nodes[x].L == 0):
-                    continue            # if list_nodes have no packets in their queue, ignore
-                
-                # list_nodes queue have packets,
-                # update their slot if they are behind current slot
-                if((self.list_nodes[x].S <= self.slot_number)):
-                    request += 1
-                    self.list_nodes[x].S = self.slot_number
-                    node_index = x
+            #if no wasted slots 
+            if(transmissions > 0):
+                if(transmissions == 1):
+                    self.success_slots = self.success_slots + 1
+                    self.list_nodes[node_index].process_packet(self.env)
+                else: #there will be collisions since transmissions is greater than 1
+                    self.collision_slots = self.collision_slots + 1
+                    for currentNode in range(NUM_NODES):
+                        if(self.list_nodes[currentNode].L > 0):
+                            if(self.list_nodes[currentNode].S == self.slot_number):
+                                #check which algorithm to use
+                                if self.algorithm == 'exponential':
+                                    self.list_nodes[currentNode].exponential_backoff()
+                                else:   
+                                    self.list_nodes[currentNode].linear_backoff()
+            else:#increment wasted slot counter
+                self.wasted_slots = self.wasted_slots + 1
+            self.slot_number = self.slot_number + 1 
 
-            # If only one node wants to transmit 
-            if request == 1:
-                self.success_slots += 1
-                
-                self.list_nodes[node_index].process_packet(self.env)
-            
-            # If more than 1 request, go through and delay each list_nodes that requested
-            elif request > 1:
-                self.collision_slots += 1
-                for x in range(NUM_NODES):
-                    if(self.list_nodes[x].S == self.slot_number) and (self.list_nodes[x].L > 0):
-                        if self.algorithm == 'exponential':
-                            self.list_nodes[x].exponential_backoff()
-                        if self.algorithm == 'linear':
-                            self.list_nodes[x].linear_backoff()
-            elif request == 0:
-                self.empty_slots += 1          
 
-            self.slot_number += 1
 
     def get_expthroughput(self):
-        print("lambda: ", self.rate, " throughput: ", self.success_slots/self.slot_number)
-        print("<success:", self.success_slots, " > <collision: ", self.collision_slots, " > <empty: ", self.empty_slots,
-                "> <total", self.slot_number, " >") 
-        exp_throughput.append(self.success_slots/self.slot_number)
+        throughput = self.success_slots/self.slot_number
+        print "Lambda: %.2f " %self.rate + "| Total Slots: %i " %self.slot_number + " | Throughput: %.4f " %throughput + " | Successful Transmissions: %i " %self.success_slots + " | Collisions: %i " %self.collision_slots + " | Wasted Slots: %i" %self.wasted_slots
+        #append throughput to exponential throughputs list
+        exp_throughput.append(throughput)
 
     def get_linearthroughput(self):
-        print("lambda: ", self.rate, " throughput: ", self.success_slots/self.slot_number)
-        print("<success:", self.success_slots, " > <collision: ", self.collision_slots, " > <empty: ", self.empty_slots,
-                "> <total", self.slot_number, " >") 
-        linear_throughput.append(self.success_slots/self.slot_number)
+        throughput = self.success_slots/self.slot_number
+        print "Lambda: %.2f " %self.rate + "| Total Slots: %i " %self.slot_number + " | Throughput: %.4f " %throughput + " | Successful Transmissions: %i " %self.success_slots + " | Collisions: %i " %self.collision_slots + " | Wasted Slots: %i" %self.wasted_slots
+        #append throughput to exponential throughputs list
+        linear_throughput.append(throughput)
                 
 
 def main():
-   
-    print("Exponential backoff")
-    for rate in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]:
+    print "\nPlease Note: Matplotlib may display errors at end of output. Please ignore, as graphs are included in hard submission.\n"
+    print "Simulation time is: %i. Slot Time T = 1. Number of Hosts N = 10\n" %SIM_TIME
+    
+    print "Exponential Backoff Results"
+    for rate in Lambda:
         env = simpy.Environment()
         myethernet = ethernet(env, rate, 'exponential')
         env.process(myethernet.start())
@@ -127,8 +126,8 @@ def main():
     print("graph will be saved as exponential_backoff.jpeg")
     print('\n')
 
-    print("Linear backoff")
-    for rate in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]:
+    print "Linear Backoff Results"
+    for rate in Lambda:
         env = simpy.Environment()
         myethernet = ethernet(env, rate, 'linear')
         env.process(myethernet.start())
